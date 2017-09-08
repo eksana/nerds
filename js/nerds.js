eecee7941a710cfe51c7ce71cbae4e4bc64276b7
Yulia Grigorenko <jvgrigorenko@gmail.com>
2017-09-09 00:43:35 +0300

    final version

--- js/nerds.js
+++ js/nerds.js
@@ -14,36 +14,36 @@ link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
   if (storage) {
- username.value = storage;
- email.focus();
-} else {
- username.focus();
-}
+    username.value = storage;
+    email.focus();
+  } else {
+    username.focus();
+  }
 });
 
 close.addEventListener("click", function (evt) {
-evt.preventDefault();
-popup.classList.remove("modal-show");
-popup.classList.remove("modal-error");
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 
 form.addEventListener("submit", function (evt) {
-if (!username.value || !email.value) {
- evt.preventDefault();
- popup.classList.remove("modal-error");
- popup.offsetWidth = popup.offsetWidth;
- popup.classList.add("modal-error");
+  if (!username.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
    } else {
-localStorage.setItem("username", username.value);
-}
+     localStorage.setItem("username", username.value);
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
-if (evt.keyCode === 27) {
-if (popup.classList.contains("modal-show")) {
-  popup.classList.remove("modal-show");
-  popup.classList.remove("modal-error");
-}
-}
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
 });
